84d3fcf1883881cd5bcb001e4cf46558b1ecda7d
megha-wadhwa12 <megha.wadhwa@kalvium.community>
2024-02-15 01:08:03 +0530

    FrontEnd-LandingPage

--- Frontend/Client-App/src/Components/Footer.jsx
+++ Frontend/Client-App/src/Components/Footer.jsx
@@ -1,29 +1,30 @@
-import { Flex, Heading, Text, Image } from "@chakra-ui/react";
+import { Flex, Heading, Text, Image } from "@chakra-ui/react"; // Importing Chakra UI components
 import React from "react";
-import Theme from "./Theme";
-import FaceBook from "./../assets/FacebookCircled.png";
-import Instagram from "./../assets/InstagramIcon.png";
-import GooglePlus from "./../assets/GooglePlus.png";
-import Twitter from "./../assets/TwitterCircled.png";
-import Youtube from "./../assets/YouTubeIcon.png";
-import Phone from "./../assets/TelephoneReceiver.png";
-import GmailIcon from "./../assets/GmailIcon.png";
-import CopyrightIcon from "./../assets/CopyrightIcon.png";
+import Theme from "./Theme"; // Importing custom theme
+import FaceBook from "./../assets/FacebookCircled.png"; // Importing Facebook icon
+import Instagram from "./../assets/InstagramIcon.png"; // Importing Instagram icon
+import GooglePlus from "./../assets/GooglePlus.png"; // Importing Google Plus icon
+import Twitter from "./../assets/TwitterCircled.png"; // Importing Twitter icon
+import Youtube from "./../assets/YouTubeIcon.png"; // Importing YouTube icon
+import Phone from "./../assets/TelephoneReceiver.png"; // Importing phone icon
+import GmailIcon from "./../assets/GmailIcon.png"; // Importing Gmail icon
+import CopyrightIcon from "./../assets/CopyrightIcon.png"; // Importing copyright icon
 
 const Footer = () => {
   return (
     <>
+      {/* Footer section */}
       <Flex
         backgroundColor={"#1E1E1E"}
         color={Theme.colors.primary[200]}
         px={10}
         pt={"70px"}
-        // textAlign=  {"center"}
-        textAlign={"left"}
+        textAlign={"left"} // Align text to left
         fontSize={"16px"}
         gap={"140px"}
         justifyContent={"center"}
       >
+        {/* Section: More About Weird Wardrobe */}
         <Flex flexDirection="column">
           <Heading size={"md"} mb={5}>
             More About Weird Wardrobe
@@ -34,6 +35,7 @@ const Footer = () => {
             outfits from around the globe.
           </Text>
         </Flex>
+        {/* Section: Stay Connected */}
         <Flex flexDirection="column">
           <Heading
             size={"md"}
@@ -44,6 +46,7 @@ const Footer = () => {
             Stay Connected
           </Heading>
           <Flex direction={"column"} gap={5}>
+            {/* Social media links */}
             <Flex alignItems={"center"} gap={6}>
               <Image src={FaceBook} width={"30px"} height={"30px"} />
               <Text>Facebook</Text>
@@ -66,18 +69,22 @@ const Footer = () => {
             </Flex>
           </Flex>
         </Flex>
+        {/* Section: Contact Information */}
         <Flex flexDirection="column" gap={6}>
           <Heading size={"md"} mb={5}>
             Contact Information
           </Heading>
           <Flex gap={6}>
+            {/* Phone contact */}
             <Image src={Phone} width={"25px"} height={"25px"} />
             <Text>Contact Us</Text>
           </Flex>
           <Flex gap={6}>
+            {/* Gmail contact */}
             <Image src={GmailIcon} width={"25px"} height={"25px"} />
             <Text>weirdwardrobe@gmail.com</Text>
           </Flex>
+          {/* Copyright */}
           <Flex gap={0} mt={250} mb={5}>
             <Image src={CopyrightIcon} width={3} />
             <Text fontSize={12}>
--- Frontend/Client-App/src/Components/Navbar.jsx
+++ Frontend/Client-App/src/Components/Navbar.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import { Box, Text, Flex, Image, Link, Button } from "@chakra-ui/react";
-import Logo from "./../assets/Weird-WardrobeLogo.png";
+import Logo from "./../assets/Weird-WardrobeLogo.png"; // Importing logo image
 
 const Navbar = () => {
   return (
     <>
+      {/* Header section with welcome message */}
       <Box backgroundColor="#BE9B85" pt={1.5} pb={1.5}>
         <Text fontFamily="welcomeFont" textAlign="center" color="white">
           WELCOME TO WEIRD WARDROBE !!
         </Text>
       </Box>
+      {/* Navbar section */}
       <Flex pt={5} pl={10}>
+        {/* Logo */}
         <Image src={Logo} alt="logo" width={"170px"} height={"28px"} mr={260} />
+        {/* Navigation links */}
         <Box
           mr={240}
           color="#BE9B85"
           fontFamily="pages"
           fontSize="sm"
-          display={{ base: "none", md: "block" }}
+          display={{ base: "none", md: "block" }} // Show links only on medium-sized screens and above
           mt={1}
         >
           <Link mr="5">HOME</Link>
@@ -25,6 +29,7 @@ const Navbar = () => {
           <Link mr="5">CONTACT US</Link>
           <Link mr="5">FAVOURITES</Link>
         </Box>
+        {/* Login button */}
         <Button
           backgroundColor="#BE9B85"
           color="white"
